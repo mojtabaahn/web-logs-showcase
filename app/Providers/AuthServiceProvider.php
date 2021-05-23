996e6a59dc8b5d42b4ec5b21c319986e266b2e9e
Mojtabaa.H.N <mojtaba.habibian@basalam.com>
2021-05-23 20:56:49 +0430

    wip

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebLogs', function ($user = null) {
+            return true;
+        });
     }
 }
